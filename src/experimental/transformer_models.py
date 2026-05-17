225d1f5525e7794e86a84ba86754710bf007fbda
Sankeerthana <Sankeerthanakoelti@gmail.com>
2026-05-17 18:43:50 +0530

    Removed transformer dependencies for deployment

--- src/experimental/transformer_models.py
+++ src/experimental/transformer_models.py
@@ -8,7 +8,7 @@ Author: Antigravity (AI Assistant)
 """
 
 try:
-    from sentence_transformers import SentenceTransformer
+    
 except (ImportError, OSError):
     SentenceTransformer = None
 import numpy as np
